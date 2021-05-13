f38a593b02e74e33c6670251bc106705f5286ea4
ryang07 <ryang07@students.cs.ubc.ca>
2021-05-13 12:30:52 -0700

    comments scraped

--- ProfDetailScraper.py
+++ ProfDetailScraper.py
@@ -10,12 +10,14 @@ def main():
     pds = ProfDetailScraper(review)
     profs = readJson()
     profs_record = []
-    students_review = []
+
     for prof in profs:
         # print(prof)
-        profs_record.append(pds.scrape_detail(prof))
+        record = pds.scrape_detail(prof)
+        profs_record.append(record)
         # print('Done')
-    writeRecordToCSV(profs_record)
+    writeToCSV(profs_record, ['Name', 'Avg Rating', 'Avg Difficulty'], './records.csv')
+    writeToCSV(students_review, ['Quality', 'Difficulty', 'Grade', "Comment"], './comments.csv')
 
 
 def readJson():
@@ -24,13 +26,14 @@ def readJson():
     return profs
 
 
-def writeRecordToCSV(profs_record):
-    with open('./records.csv', 'w') as file:
-        header = ['Name', 'Avg Rating', 'Avg Difficulty']
+def writeToCSV(data, header, path):
+    with open(path, 'w') as file:
         writer = csv.DictWriter(file, fieldnames=header)
         writer.writeheader()
-        for record in profs_record:
-            dict_prof = {'Name': record[0], 'Avg Rating': record[1], 'Avg Difficulty': record[2]}
+        for data_row in data:
+            dict_prof = {}
+            for i in range(len(header)):
+                dict_prof[header[i]] = data_row[i]
             writer.writerow(dict_prof)
 
 
@@ -67,19 +70,36 @@ class ProfDetailScraper:
         # print(record[0], " ", record[1], " ", record[2])
 
         counter = 0
-        review = ['', '', '', '']
+        grade = 0
         regex_body = re.compile('^Rating__RatingBody.*')
         regex_num = re.compile('^CardNumRating__CardNumRatingNumber.*')
+        regex_meta = re.compile('^CourseMeta__StyledCourseMeta.*')
+        regex_grade = re.compile('.*Grade.*')
+        regex_comment = re.compile('^Comments__StyledComments.*')
         ratings_raw = soup.find_all('div', {'class': regex_body})
+
         # print(len(ratings_raw))
         for rating_raw in ratings_raw:
+            self.review = ['', '', '', '']
             for r in rating_raw.find_all('div', {'class': regex_num}):
-                review[counter % 2] = r.text
+                self.review[counter % 2] = r.text
                 # if counter % 2 == 1:
                 # print(review[0], ' ', review[1], ' ', review[2], ' ', review[3])
                 counter = counter + 1
+            for r in rating_raw.find('div', {'class': regex_meta}).contents:
+                hasGrade = r.find(text=regex_grade)
+                if hasGrade:
+                    grade = hasGrade.parent.find('span').text
+                if len(str(grade)) > 2:
+                    grade = 'N/A'
+                self.review[2] = grade
+            for r in rating_raw.find('div', {'class': regex_comment}):
+                self.review[3] = str(r)
+
+            students_review.append(self.review)
 
         return record
 
 
+students_review = []
 main()
